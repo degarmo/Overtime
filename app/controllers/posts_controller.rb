73dfee2d62dcd42f3e4d3601875a9bfcec5aff6a
Cory <degarmo@gmail.com>
2018-10-18 17:55:20 -0500

    added post show page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,22 @@
 class PostsController < ApplicationController
-  
 
   def index
 
   end
+
+  def new
+      @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:date, :rational))
+
+    @post.save
+    redirect_to @posts
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
 end
